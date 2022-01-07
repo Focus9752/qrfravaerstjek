54a5582746de0fbe2aa99249a8c7dec54d83be5e
Focus9752 <50440281+Focus9752@users.noreply.github.com>
2022-01-07 19:24:15 +0100

    .

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-# læs app
+import cv2
+from pyzbar import pyzbar
+import pyttsx3
+from playsound import playsound
+
+seennames = []
+
+def read_barcodes(frame):
+    barcodes = pyzbar.decode(frame)
+    for barcode in barcodes:
+        x, y , w, h = barcode.rect
+        #1
+        barcode_info = barcode.data.decode('utf-8')
+        cv2.rectangle(frame, (x, y),(x+w, y+h), (0, 255, 0), 2)
+        
+        #2
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, barcode_info, (x + 6, y - 6), font, 2.0, (255, 255, 255), 1)
+        #3
+        
+        if barcode_info not in seennames:
+            seennames.append(barcode_info)
+
+            with open("barcode_result.txt", mode ='w') as file:
+                file.write(str(seennames))
+            
+            engine = pyttsx3.init()
+            engine.say(barcode_info + "has checked in.")
+            engine.runAndWait()
+    return frame
+
+def main():
+    #1
+    camera = cv2.VideoCapture(0)
+    ret, frame = camera.read()
+    #2
+    while ret:
+        ret, frame = camera.read()
+        frame = read_barcodes(frame)
+        cv2.imshow('Barcode/QR code reader', frame)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+    #3
+    camera.release()
+    cv2.destroyAllWindows()
+#4
+if __name__ == '__main__':
+    main()
+
+
