8d127fd61414d43b98019c7db6918215c3055736
Focus9752 <50440281+Focus9752@users.noreply.github.com>
2022-01-17 14:07:15 +0100

    .

--- main.py
+++ main.py
@@ -6,36 +6,41 @@ import json
 
 seennames = []
 
-# Stores s
-
 studentsDict = {
-    ("Alberte Cort"): False,
-    ("Anton Lauve Hermann"): False,
-    ("Clara Thorsø Kousgaard"): False,
-    ("Ella Petersen"): False,
-    ("Eniola Felix Ladipo Oyebo"): False,
-    ("Frederik Elstrøm Hansen"): False,
-    ("Frederikke Ankjær Andersen"): False,
-    ("Hector Chamero Andersen"): False,
-    ("Josefine Victoria Wildschiødtz"): False,
-    ("Lasse West Holstvig"): False,
-    ("Lucca Vibeke Prühs"): False,
-    ("Marcus Koschmieder Krogsgaard"): False,
-    ("Maya Oh Holmen"): False,
-    ("Oliver Christian Glæsel Fugmann"): False,
-    ("Pauline Baden Munkholm"): False,
-    ("Philip Søholt"): False,
-    ("Samuel Alvarez Grønbech-Jensen"): False,
-    ("Thor Marner"): False
+    "3a 02": ["Alberte Cort", "false"],
+    "3a 31": ["Anton Lauve Hermann", "false"],
+    "3a 07": ["Clara Thorsø Kousgaard", "false"],
+    "3a 08": ["Ella Petersen", "false"],
+    "3a 29": ["Eniola Felix Ladipo Oyebo", "false"],
+    "3a 10": ["Frederik Elstrøm Hansen", "false"],
+    "3a 11": ["Frederikke Ankjær Andersen", "false"],
+    "3a 12": ["Hector Chamero Andersen", "false"],
+    "3a 13": ["Josefine Victoria Wildschiødtz", "false"],
+    "3a 14": ["Lasse West Holstvig", "false"],
+    "3a 30": ["Lucca Vibeke Prühs", "false"],
+    "3a 16": ["Marcus Koschmieder Krogsgaard", "false"],
+    "3a 17": ["Maya Oh Holmen", "false"],
+    "3a 20": ["Oliver Christian Glæsel Fugmann", "false"],
+    "3a 21": ["Pauline Baden Munkholm", "false"],
+    "3a 22": ["Philip Søholt", "false"],
+    "3a 25": ["Samuel Alvarez Grønbech-Jensen", "false"],
+    "3a 26": ["Thor Marner", "false"]
 }
 
+def updateJSON():
+    for name in seennames:
+        studentsDict[name][1] = "true"
+    
+    with open('students.json', 'w') as outfile:
+        json.dump(studentsDict, outfile)
+
 def read_barcodes(frame):
-    # Læs det nuværende billede fra kameraet og led efter QR-koder
+    # Læs den nuværende frame fra kameraet og led efter QR-koder
     barcodes = pyzbar.decode(frame)
 
-    # For hver QR-kode som vi ser i billedet
+    # For hver QR-kode som vi ser
     for barcode in barcodes:
-        # Koordinaterne til QR-koden i billedet
+        # Koordinaterne til QR-koden på skærmen
         x, y , w, h = barcode.rect
         
         # Aflæs QR-koden (antag at den indeholder tekst i utf-8 format)
@@ -52,31 +57,31 @@ def read_barcodes(frame):
             # Tilføj det til listen af sete navne
             seennames.append(barcode_info)
 
-            # Opdater filen med de sete navne
-            with open("barcode_result.txt", mode ='w') as file:
-                file.write(str(seennames))
+            updateJSON()
             
-            # 
+            # Kom med tts-melding om at eleven har tjekket ind
             engine = pyttsx3.init()
-            engine.say(barcode_info + "has checked in.")
+            engine.say(studentsDict[barcode_info][0] + "has checked in.")
             engine.runAndWait()
     return frame
 
 def main():
-    #1
+    # Hent frame fra kameraet med openCV2-biblioteket
     camera = cv2.VideoCapture(0)
     ret, frame = camera.read()
-    #2
+    
+    # Led efter QR-koder i billedet
     while ret:
         ret, frame = camera.read()
         frame = read_barcodes(frame)
         cv2.imshow('Barcode/QR code reader', frame)
         if cv2.waitKey(1) & 0xFF == 27:
             break
-    #3
+    
+    # Luk programmet hvis brugeren trykker esc
     camera.release()
     cv2.destroyAllWindows()
-#4
+
 if __name__ == '__main__':
     main()
 
