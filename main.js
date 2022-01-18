28e122c7d0cd200feda57e07cf725c1c8ac7ca97
Focus9752 <50440281+Focus9752@users.noreply.github.com>
2022-01-18 14:43:09 +0100

    .

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
+//Indlæs dele af hjemmesiden
+let lastRefreshTimeSpan = document.getElementById("lastRefreshTime");
+let attendanceRatio_studentNumber = document.getElementById("attendanceRatio_studentNumber");
 
+let studentsJSON = JSON.parse("./students.json");
+console.log(studentsJSON);
+
+//Gør forskellige ting og sager når siden indlæses
+onload = function() {
+    lastRefreshTimeSpan.innerHTML = "Opdateret " + prettyDate()
+}
 
 function updateAttendance() {
-
+    lastRefreshTimeSpan.innerHTML = "Opdateret " + prettyDate()
 }
 
 function prettyDate() {
     var dateWithouthSecond = new Date();
     return dateWithouthSecond.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'});
 }
+
