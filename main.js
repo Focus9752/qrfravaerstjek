29be6f21b887728212d24a4893577654cd296a80
Focus9752 <50440281+Focus9752@users.noreply.github.com>
2022-01-19 12:32:49 +0100

    fjernet en masse ting :(

--- main.js
+++ main.js
@@ -2,13 +2,20 @@
 let lastRefreshTimeSpan = document.getElementById("lastRefreshTime");
 let attendanceRatio_studentNumber = document.getElementById("attendanceRatio_studentNumber");
 
-let studentsJSON = JSON.parse("./students.json");
-console.log(studentsJSON);
+let studentsJSON = {}
 
 //Gør forskellige ting og sager når siden indlæses
-onload = function() {
+onload = setTimeout(() => {
     lastRefreshTimeSpan.innerHTML = "Opdateret " + prettyDate()
-}
+    $(document).ready(function(){
+        $.getJSON("/examples/data/test.json", function(data){
+            studentsJSON = data;
+            console.log(studentsJSON);
+        }).fail(function(){
+            document.write("An error has occurred.");
+        });
+    });
+}, 50);
 
 function updateAttendance() {
     lastRefreshTimeSpan.innerHTML = "Opdateret " + prettyDate()
@@ -18,4 +25,5 @@ function prettyDate() {
     var dateWithouthSecond = new Date();
     return dateWithouthSecond.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'});
 }
+
 
--- main.py
+++ main.py
@@ -1,37 +1,41 @@
+from cgitb import text
 import cv2
 from pyzbar import pyzbar
 import pyttsx3
-import json
+import pandas
+import cv2
+import PySimpleGUI as sg
 
 seennames = []
 
 studentsDict = {
-    "3a 02": ["Alberte Cort", "false"],
-    "3a 31": ["Anton Lauve Hermann", "false"],
-    "3a 07": ["Clara Thorsø Kousgaard", "false"],
-    "3a 08": ["Ella Petersen", "false"],
-    "3a 29": ["Eniola Felix Ladipo Oyebo", "false"],
-    "3a 10": ["Frederik Elstrøm Hansen", "false"],
-    "3a 11": ["Frederikke Ankjær Andersen", "false"],
-    "3a 12": ["Hector Chamero Andersen", "false"],
-    "3a 13": ["Josefine Victoria Wildschiødtz", "false"],
-    "3a 14": ["Lasse West Holstvig", "false"],
-    "3a 30": ["Lucca Vibeke Prühs", "false"],
-    "3a 16": ["Marcus Koschmieder Krogsgaard", "false"],
-    "3a 17": ["Maya Oh Holmen", "false"],
-    "3a 20": ["Oliver Christian Glæsel Fugmann", "false"],
-    "3a 21": ["Pauline Baden Munkholm", "false"],
-    "3a 22": ["Philip Søholt", "false"],
-    "3a 25": ["Samuel Alvarez Grønbech-Jensen", "false"],
-    "3a 26": ["Thor Marner", "false"]
+    "3a 02": ["Alberte Cort", "Fraværende"],
+    "3a 31": ["Anton Lauve Hermann", "Fraværende"],
+    "3a 07": ["Clara Thorsø Kousgaard", "Fraværende"],
+    "3a 08": ["Ella Petersen", "Fraværende"],
+    "3a 29": ["Eniola Felix Ladipo Oyebo", "Fraværende"],
+    "3a 10": ["Frederik Elstrøm Hansen", "Fraværende"],
+    "3a 11": ["Frederikke Ankjær Andersen", "Fraværende"],
+    "3a 12": ["Hector Chamero Andersen", "Fraværende"],
+    "3a 13": ["Josefine Victoria Wildschiødtz", "Fraværende"],
+    "3a 14": ["Lasse West Holstvig", "Fraværende"],
+    "3a 30": ["Lucca Vibeke Prühs", "Fraværende"],
+    "3a 16": ["Marcus Koschmieder Krogsgaard", "Fraværende"],
+    "3a 17": ["Maya Oh Holmen", "Fraværende"],
+    "3a 20": ["Oliver Christian Glæsel Fugmann", "Fraværende"],
+    "3a 21": ["Pauline Baden Munkholm", "Fraværende"],
+    "3a 22": ["Philip Søholt", "Fraværende"],
+    "3a 25": ["Samuel Alvarez Grønbech-Jensen", "Fraværende"],
+    "3a 26": ["Thor Marner", "Fraværende"]
 }
 
-def updateJSON():
+def updateFile():
     for name in seennames:
-        studentsDict[name][1] = "true"
+        studentsDict[name][1] = "Til stede"
     
-    with open('students.json', 'w') as outfile:
-        json.dump(studentsDict, outfile)
+    df = pandas.DataFrame(data=studentsDict)
+    df.to_excel("fravær.xlsx")
+    
 
 def read_barcodes(frame):
     # Læs den nuværende frame fra kameraet og led efter QR-koder
@@ -45,18 +49,18 @@ def read_barcodes(frame):
         # Aflæs QR-koden (antag at den indeholder tekst i utf-8 format)
         barcode_info = barcode.data.decode('utf-8')
         # Tegn en grøn kant rundt om QR-koden
-        cv2.rectangle(frame, (x, y),(x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y),(x+w, y+h), (0, 0, 255), 2)
         
         # Vis teksten fra QR koden oven på koden
         font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, barcode_info, (x + 6, y - 6), font, 2.0, (255, 255, 255), 1)
+        cv2.putText(frame, barcode_info, (x + 6, y - 6), font, 2.0, (0, 0, 255), 1)
         
         # Hvis vi ikke har set navnet før
         if barcode_info not in seennames:
             # Tilføj det til listen af sete navne
             seennames.append(barcode_info)
 
-            updateJSON()
+            updateFile()
             
             # Kom med tts-melding om at eleven har tjekket ind
             engine = pyttsx3.init()
@@ -64,24 +68,13 @@ def read_barcodes(frame):
             engine.runAndWait()
     return frame
 
-def main():
-    # Hent frame fra kameraet med openCV2-biblioteket
-    camera = cv2.VideoCapture(0)
-    ret, frame = camera.read()
-    
-    # Led efter QR-koder i billedet
-    while ret:
-        ret, frame = camera.read()
-        frame = read_barcodes(frame)
-        cv2.imshow('Barcode/QR code reader', frame)
-        if cv2.waitKey(1) & 0xFF == 27:
-            break
-    
-    # Luk programmet hvis brugeren trykker esc
-    camera.release()
-    cv2.destroyAllWindows()
-
-if __name__ == '__main__':
-    main()
-
+window = sg.Window('Demo Application - OpenCV Integration', [[sg.Image(filename='', key='image')],], location=(800,400))
+camera = cv2.VideoCapture(0)       # Setup the camera as a capture device
+while True:                     # The PSG "Event Loop"
+    event, values = window.Read(timeout=20, timeout_key='timeout')      # get events for the window with 20ms max wait
+    if event is None:  
+        break                                            # if user closed window, quit
+    cv2image= cv2.cvtColor(camera.read()[1],cv2.COLOR_BGR2RGB)
+    frame = read_barcodes(cv2image)
+    window.find_element('image').Update(data=cv2.imencode('.png', frame)[1].tobytes()) # Update image in window
 
